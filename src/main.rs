8d3f2389184d97fa11ec17827f7d076b7dab95f2
Wataru Kurashima <what.r.j150@gmail.com>
2021-11-09 06:24:03 +0900

    feat: 🎸 get holidays as Vec<Holiday>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use chrono::{Local};
+use serde_derive::{Deserialize, Serialize};
+
+pub type Holidays = Vec<Holiday>;
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Holiday {
+    pub date: String,
+    pub name: String,
+}
 
 #[async_std::main]
 async fn main() {
     let thisyear: String = Local::today().format("%Y").to_string();
     let uri = format!("{}{}", "https://api.national-holidays.jp/", thisyear);
-    let mut res = surf::get(uri).await.unwrap();
-    println!("{}", res.body_string().await.unwrap());
+    let holidays: Holidays = surf::get(uri).recv_json().await.unwrap();
+    dbg!(holidays);
 }
