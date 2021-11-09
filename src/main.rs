e2f06f8183b30ec59f6a68f86acba0d425f20515
Wataru Kurashima <what.r.j150@gmail.com>
2021-11-09 13:05:47 +0900

    feat: 🎸 judge today

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,15 @@ pub struct Holiday {
 #[async_std::main]
 async fn main() {
     let thisyear: String = Local::today().format("%Y").to_string();
+    let today: String = Local::today().format("%Y-%m-%d").to_string();
     let uri = format!("{}{}", "https://api.national-holidays.jp/", thisyear);
     let holidays: Holidays = surf::get(uri).recv_json().await.unwrap();
-    dbg!(holidays);
+    let iter = holidays.iter();
+    let mut is_holiday = false;
+    for holiday in iter {
+        if holiday.date == today {
+            is_holiday = true;
+        }
+    }
+    dbg!(is_holiday);
 }
